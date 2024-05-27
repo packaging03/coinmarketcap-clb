6b640536101d743eee73d05e01326b9780120e30
Adeyemi Olaoye <yhemi06@gmail.com>
2024-05-27 08:56:16 +0100

    added assets, header component and configure tailwind

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+// export default nextConfig;
+const nextConfig = {
+  images: {
+    domains: [
+      "s2.coinmarketcap.com",
+      "s3.coinmarketcap.com",
+      "encrypted-tbn0.gstatic.com",
+    ],
+  },
+};
 
 export default nextConfig;
